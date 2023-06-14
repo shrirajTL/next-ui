cdbe03590435b08e6da8e826cfcb42d2cf4b9656
shrirajTL <shriraj@talkinglands.com>
2023-06-14 09:21:18 +0530

    modal

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,25 @@
 import type { NextPage } from 'next'
-import { Container, Navbar, Text, Button, Grid, Col } from '@nextui-org/react';
+import { Container, Navbar, Text, Button, Grid, Col, Modal, Input, Row, Checkbox, } from '@nextui-org/react';
 import InfoCard from '../components/InfoCard';
+import { useRouter } from 'next/router';
+//import ModalComp from '../components/Modal';
+import { useState } from 'react';
+
 
 const Home: NextPage = () => {
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push('https://qa.talkinglands.com');
+  }
+
+  const [visible, setVisible] = useState(false);
+  const handler = () => setVisible(true);
+  const closeHandler = () => {
+    setVisible(false);
+    console.log("closed");
+  };
+
   return (
     <Container>
       <Navbar isCompact variant={"static"}>
@@ -19,19 +36,63 @@ const Home: NextPage = () => {
         <Navbar.Content>
           <Navbar.Link href="#">Login</Navbar.Link>
           <Navbar.Item>
-            <Button auto flat color='error' href="#">
-              Sign Up
-            </Button> 
+            <div>
+              <Button auto flat color='error' onPress={handler}>
+                Sign Up
+              </Button>
+              <Modal
+                closeButton
+                blur
+                aria-labelledby="modal-title"
+                open={visible}
+                onClose={closeHandler}
+              >
+                <Modal.Body>
+                  <Input
+                    clearable
+                    bordered
+                    fullWidth
+                    color="primary"
+                    size="lg"
+                    placeholder="Email"
+                  />
+                  <Input
+                    clearable
+                    bordered
+                    fullWidth
+                    color="primary"
+                    size="lg"
+                    placeholder="Password"
+                  />
+                  <Row justify="space-between">
+                    <Checkbox>
+                      <Text size={14}>Remember me</Text>
+                    </Checkbox>
+                    <Text size={14}>Forgot password?</Text>
+                  </Row>
+                </Modal.Body>
+                <Modal.Footer>
+                  <Button auto flat color="error" onPress={closeHandler}>
+                    Close
+                  </Button>
+                  <Button auto onPress={closeHandler}>
+                    Sign in
+                  </Button>
+                </Modal.Footer>
+              </Modal>
+            </div>
           </Navbar.Item>
         </Navbar.Content>
       </Navbar>
-    
-      <Grid.Container justify="center" css={{"height": "500px", "backgroundColor": "black"}}>
+
+      <Grid.Container justify="center" css={{ "height": "500px", "backgroundColor": "black" }}>
         <Grid xs={12} sm={6} alignItems="center">
-          <Col css={{"width": "100%"}}>
-            <Text weight={"bold"} size={70} css={{"textAlign": "center"}}>Revolutionizing</Text>
-            <Text weight={"bold"} size={70} css={{"textAlign": "center"}}>Real Estate</Text>
-            <Button size="lg" shadow color="error" css={{"width": "100%", "marginTop": "10px"}}>SIGN IN</Button>
+          <Col css={{ "width": "100%" }}>
+            <Text weight={"bold"} size={70} css={{ "textAlign": "center" }}>Revolutionizing</Text>
+            <Text weight={"bold"} size={70} css={{ "textAlign": "center" }}>Real Estate</Text>
+            <Button onClick={handleClick} size="lg" shadow color="error" css={{ "width": "100%", "marginTop": "10px" }}>
+              Explore Properties
+            </Button>
           </Col>
         </Grid>
       </Grid.Container>
